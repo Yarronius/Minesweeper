179ddba5856ab4e6dfcb44ae09bcbd7f6ee6ac18
Yaroslav Kyslyi <yarronius@gmail.com>
2023-09-20 14:39:07 +0300

    init project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.minesweeper {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.minesweeper to javafx.fxml;
+    exports com.example.minesweeper;
+}

721aa4d2eb4b6dd3dccccb08703aa47ca14780d4
Yaroslav Kyslyi <yarronius@gmail.com>
2023-09-28 20:55:43 +0300

    launcher class added, list bug fixed

--- src/main/java/com/example/minesweeper/View.java
+++ src/main/java/com/example/minesweeper/View.java
@@ -24,6 +24,12 @@ public class View extends GridPane {
     private ComboBox<Integer> comboBoxWidth = new ComboBox<Integer>();;
     private ComboBox<Integer> comboBoxHeight = new ComboBox<Integer>();
 
+    {
+        comboBoxWidth.getItems().addAll(5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+                31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60);
+        comboBoxHeight.getItems().addAll(5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30);
+    }
+
     public void initialize(Tile[][] gameField) {
         getChildren().clear();
         getColumnConstraints().clear();
@@ -73,8 +79,6 @@ public class View extends GridPane {
         hBox2.setAlignment(Pos.CENTER);
 
         comboBoxWidth.disableProperty().setValue(true);
-        comboBoxWidth.getItems().addAll(5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-                                                    31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60);
         comboBoxWidth.getSelectionModel().selectFirst();
         comboBoxWidth.setConverter(new IntegerStringConverter());
         comboBoxWidth.setPrefWidth(55.0);
@@ -82,7 +86,6 @@ public class View extends GridPane {
         Text textHeight = new Text("Высота:    ");
 
         comboBoxHeight.disableProperty().setValue(true);
-        comboBoxHeight.getItems().addAll(5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30);
         comboBoxHeight.getSelectionModel().selectFirst();
         comboBoxHeight.setConverter(new IntegerStringConverter());
         comboBoxHeight.setPrefWidth(55.0);
